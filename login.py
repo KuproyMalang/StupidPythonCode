fa9a31c5f038da05da76de36f7c9b682807b0a41
HapidKicoKacoKocokKocokPeler <82764920+KuproyMalang@users.noreply.github.com>
2021-11-02 05:24:42 +0700

    Stupid Beginner Register Login
    help me to make better

---
+++ login.py
@@ -0,0 +1,22 @@
+print("================Register=================")
+
+x = input("Username : ")
+y = input("Password : ")
+
+print("===============Login=====================")
+
+a = input("Username : ")
+b = input("Password : ")
+
+if b == y:
+    if a == x:
+        print("Welcome ^_^")
+    else:
+        print("Wrong!!!")
+elif b != y:
+    print("Wrong!!!")
+else:
+    print("Wrong!!!")
+
+
+
